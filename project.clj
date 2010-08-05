bba5bd58c0f59f01e27d0a55de91acf9332f6053
Matt Courtney <macourtney@gmail.com>
2010-08-05 13:16:09 -0400

    Added clojars dependency.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject drift "1.0.0-SNAPSHOT"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]])
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies [[lein-clojars "0.5.0"]])
