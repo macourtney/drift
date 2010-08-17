01929ce448fde364c4699ba733ccc55712028873
Matthew Courtney <mcourtne199@yahoo.com>
2010-08-17 19:43:54 -0400

    Updated to version 1.0.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject drift "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject drift "1.0.0"
+  :description "Drift is a rails like migration framework for Clojure."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]])
