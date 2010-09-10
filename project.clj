5e7691c66c03876bd4f82ee5d0d93f46cc2c9c63
Matt Courtney <macourtney@gmail.com>
2010-09-10 14:03:51 -0400

    Updated to the lastest version of Clojure and set the version to 1.1.0-RC1.

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject drift "1.1.0-SNAPSHOT"
+(defproject drift "1.1.0-RC1"
   :description "Drift is a rails like migration framework for Clojure."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-                 [org.clojure/clojure-contrib "1.1.0"]]
-  :dev-dependencies [[lein-clojars "0.5.0"]])
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
