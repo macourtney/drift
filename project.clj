e487506b991c4332a2873554b299fbc5a7f5f684
Matt Courtney <macourtney@gmail.com>
2011-05-21 15:50:09 -0400

    Updated the version number to a snapshot version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject drift "1.2.0"
+(defproject drift "1.2.1-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
   :dependencies [[clojure-tools "1.0.0"]
                  [org.clojure/clojure "1.2.0"]
