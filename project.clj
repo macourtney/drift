2638eb3e69c328289ab48b0760ffa1d8d052cb56
Matt Courtney <macourtney@gmail.com>
2011-11-27 11:31:07 -0500

    Updated all versions of all dependencies to the latest version.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject drift "1.4.2-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
-  :dependencies [[clojure-tools "1.0.2"]
-                 [org.clojure/clojure "1.2.1"]
-                 [org.clojure/tools.logging "0.2.0"]]
+  :dependencies [[clojure-tools "1.0.3"]
+                 [org.clojure/clojure "1.3.0"]
+                 [org.clojure/tools.logging "0.2.3"]]
   :dev-dependencies [[log4j/log4j "1.2.16"]]
 
   :aot [drift.Drift])
