5a9165efb7a43edd7714f78077d8847d05cd94c6
Jeff Rose <rosejn@gmail.com>
2012-06-17 12:14:52 +0100

    Adding documentation strings for lein help.

--- src/leiningen/create_migration.clj
+++ src/leiningen/create_migration.clj
@@ -2,6 +2,7 @@
   (:require [leiningen.compile :as lein-compile]))
 
 (defn create-migration [project & args]
+  "Create a new migration file."
   (lein-compile/eval-in-project project
     `(drift.generator/generate-migration-file ~(first args))
     nil nil '(require 'drift.generator)))
--- src/leiningen/migrate.clj
+++ src/leiningen/migrate.clj
@@ -2,6 +2,7 @@
   (require [leiningen.compile :as lein-compile]))
 
 (defn migrate [project & args]
+  "Run migration scripts."
   (lein-compile/eval-in-project project
     `(drift.execute/run '~args)
     nil nil '(require 'drift.execute)))
