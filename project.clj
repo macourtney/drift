8a3011839515ccc68871894f17885a61a4107d03
Matt Courtney <macourtney@gmail.com>
2012-11-20 17:07:46 -0500

    Removed the clojure reference in the project.clj file.

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject drift "1.4.6-SNAPSHOT"
   :description "Drift is a rails like migration framework for Clojure."
   :dependencies [[clojure-tools "1.1.2"]
-                 [org.clojure/clojure "1.4.0"]
                  [org.clojure/tools.logging "0.2.3"]]
   :profiles {:dev {:dependencies [[log4j/log4j "1.2.16"]]}}
 
